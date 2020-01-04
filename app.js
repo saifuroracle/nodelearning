cf9ea007de4851d24252c7ea5c375e47ee0e49f5
saifur rahman <saifuroracle@gmail.com>
2020-01-04 22:29:08 +0600

    hello world

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 var port = 8080;
+var db = "mongodb://localhost/mongodblearning";
+
+// models
+var books = require("./model/Book.model");
+
+// db connect
+mongoose.connect(db);
+
+app.get("/", (req, res) => {
+    res.send("Hello world !");
+});
 
 app.listen(port, function() {
     console.log('app listening on port ' + port);
---
+++ model/Book.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model("Book", BookSchema);
