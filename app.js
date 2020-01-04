70a682395026b5fd5725999b99353ee38bd5c35d
saifur rahman <saifuroracle@gmail.com>
2020-01-04 22:55:58 +0600

    deprecated resolved

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 var port = 8080;
 var db = "mongodb://localhost/mongodblearning";
-
+const options = {
+    keepAlive: 1,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
 // models
 var books = require("./model/Book.model");
 
 // db connect
-mongoose.connect(db);
+mongoose.connect(db, options)
+    .then(() => console.log("💻 Mondodb Connected"))
+    .catch(err => console.error(err));
 
 app.get("/", (req, res) => {
     res.send("Hello world !");
